06d768e680a2bb59d65ebeeffe05125e0a835a37
Stacy Austin <saustin4@radford.edu>
2017-04-02 14:36:48 -0400

    GET & POST to/from db complete

--- app/src/main/java/com/example/jona1/mypet/content_main_screen.java
+++ app/src/main/java/com/example/jona1/mypet/content_main_screen.java
@@ -1,20 +1,31 @@
 package com.example.jona1.mypet;
 
 
-import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+public class content_main_screen extends AppCompatActivity {
 
-public class content_main_screen extends Activity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main_screen);
 
+        textView = (TextView) findViewById(R.id.textViewUserName);
+
+        Intent intent = getIntent();
+
+        String username = intent.getStringExtra(LoginActivity.USER_NAME);
+
+
+        // Jonathan "welcome back" will need to be converted to a string resource to solve the warning
+        // I forgot how to do it
+        textView.setText("Welcome back, "+username);
     }
+
 }
-
-
-
