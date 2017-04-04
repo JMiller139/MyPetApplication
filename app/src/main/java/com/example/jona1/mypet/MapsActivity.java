2c1db3c547e85b03485ea5b3e351310732319ce3
MickeyIT25 <btadesse@radford.edu>
2017-04-04 15:34:00 -0400

    Added Info Tab

--- app/src/main/java/com/example/jona1/mypet/MapsActivity.java
+++ app/src/main/java/com/example/jona1/mypet/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -28,10 +29,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.setMyLocationEnabled(true);
+        LatLng radford = new LatLng(37.1385231, -80.5483627);
+        mMap.addMarker(new MarkerOptions().position(radford).title("Marker of Radford University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(radford));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(radford).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
