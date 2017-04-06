fc16c9af98e002f43d61f3a6afe82803b0587616
Paige Ludecker <pludecker@radford.edu>
2017-04-06 18:49:04 -0400

    CreatePetProfile added DB connectivity -Paige

--- app/src/main/java/com/example/jona1/mypet/CreatePetProfileActivity.java
+++ app/src/main/java/com/example/jona1/mypet/CreatePetProfileActivity.java
@@ -1,13 +1,17 @@
 package com.example.jona1.mypet;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
+import java.util.HashMap;
 
-public class CreatePetProfileActivity extends AppCompatActivity {
+public class CreatePetProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText petName;
     private EditText species;
@@ -17,61 +21,113 @@ public class CreatePetProfileActivity extends AppCompatActivity {
     private Switch bite;
     private EditText notes;
 
+    private Button buttonRegister;
+
+    private static final String REGISTER_URL = "https://php.radford.edu/~team04/userRegistration/register.php";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pet_profile);
-    }
 
-    public void addPetClicked(View v){
-        updateInfo();
-        String pName = getPetName();
-        String pSpecies = getSpecies();
-        String pBreed = getBreed();
-        String pMarkings = getMarkings();
-        String pRabies = getRabies();
-        Boolean pBite = getBite();
-        String pNotes = getNotes();
-    }
-
-    public void updateInfo(){
-        this.petName = (EditText) findViewById(R.id.petNameTextView);
+        this.petName = (EditText) findViewById(R.id.petNameTextField);
         this.species = (EditText) findViewById(R.id.speciesEditText);
         this.breed = (EditText) findViewById(R.id.breedEditText);
         this.markings = (EditText) findViewById(R.id.markingsEditText);
         this.rabiesTag = (EditText) findViewById(R.id.rabiesEditText);
         this.bite = (Switch) findViewById(R.id.biteSwitch);
         this.notes = (EditText) findViewById(R.id.notesSection);
+
+        buttonRegister = (Button) findViewById(R.id.createPetButton);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            addPetClicked();
+        }
+    }
+
+    public void addPetClicked(){
+        String pName = getPetName();
+        String pSpecies = getSpecies();
+        String pBreed = getBreed();
+        String pMarkings = getMarkings();
+        String pRabies = getRabies();
+        String pBite = getBite();
+        String pNotes = getNotes();
+
+        register(pName, pSpecies, pBreed, pMarkings, pRabies, pBite, pNotes);
+    }
+
+    private void register(String pName, String pSpecies, String pBreed, String pMarkings,
+                          String pRabies, String pBite, String pNotes) {
+        class RegisterUser extends AsyncTask<String, Void, String>{
+            private ProgressDialog loading;
+            private RegisterUserClass ruc = new RegisterUserClass();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(CreatePetProfileActivity.this, "Please Wait",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<>();
+                data.put("fname",params[0]);
+                data.put("lname",params[1]);
+                data.put("address",params[2]);
+                data.put("username",params[3]);
+                data.put("password",params[4]);
+                data.put("email",params[5]);
+
+                return  ruc.sendPostRequest(REGISTER_URL,data);
+            }
+        }
+
+        RegisterUser ru = new RegisterUser();
+        ru.execute(pName,pSpecies,pBreed,pMarkings,pRabies,pBite,pNotes); //Pass bite as a string
     }
 
     private String getPetName(){
-        return petName.getText().toString();
+        return petName.getText().toString().trim().toLowerCase();
     }
 
     private String getSpecies(){
-        return species.getText().toString();
+        return species.getText().toString().trim().toLowerCase();
     }
 
     private String getBreed(){
-        return breed.getText().toString();
+        return breed.getText().toString().trim().toLowerCase();
     }
 
     private String getMarkings(){
-        return markings.getText().toString();
+        return markings.getText().toString().trim().toLowerCase();
     }
 
     private String getRabies(){
-        return rabiesTag.getText().toString();
+        return rabiesTag.getText().toString().trim().toLowerCase();
     }
 
-    private Boolean getBite(){
-        return bite.isChecked();
+    private String getBite(){
+        if (getBite) {return "true";}
+        else {return "false";}
     }
 
     private String getNotes(){
-        return notes.getText().toString();
+        return notes.getText().toString().trim().toLowerCase();
     }
 
 
