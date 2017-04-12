b6d8c5a400cdc4303d2d98e0f76bb1f1540265ee
JMiller139 <jmiller517@radford.edu>
2017-04-11 20:26:40 -0400

    stuff

--- app/src/main/java/com/example/jona1/mypet/EditProfileActivity.java
+++ app/src/main/java/com/example/jona1/mypet/EditProfileActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-
-import java.sql.*;
+import android.util.Log;
 
 
 public class EditProfileActivity extends AppCompatActivity implements View.OnClickListener{
@@ -15,6 +14,8 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
     private String lname;
     private String address;
     private String email;
+
+    private static String TAG = "testingMessage";
 
 
     @Override
@@ -29,36 +30,8 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View v) {
         if(v == buttonRegister){
-            String url = "jdbc:mysql://192.168.1.102:3306/Team04";
+            getUserInformation();
 
-
-            try {
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-                Connection conn = DriverManager.getConnection(url, "Team04", "team04");
-                String query = "SELECT * FROM users WHERE user_id = 13";
-                Statement st = conn.createStatement();
-                ResultSet rs = st.executeQuery(query);
-                while (rs.next())
-                {
-                    fname = rs.getString("fname");
-                    lname = rs.getString("lname");
-                    address = rs.getString("address");
-                    email = rs.getString("email");
-
-                    EditText ed = (EditText) findViewById (R.id.changeFName);
-                    ed.setText(fname);
-                    ed = (EditText) findViewById (R.id.changeLName);
-                    ed.setText(fname);
-                    ed = (EditText) findViewById (R.id.changeEmail);
-                    ed.setText(fname);
-                    ed = (EditText) findViewById (R.id.changeAddress);
-                    ed.setText(fname);
-                }
-                st.close();
-
-            } catch (SQLException | ClassNotFoundException | IllegalAccessException | InstantiationException e1) {
-                e1.printStackTrace();
-            }
         }
     }
 }
